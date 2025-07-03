7ad9d1e2164f3d9cc726cee2f4e50fca9ad860c5
S Harshavardhan <harsha.echo348@gmail.com>
2025-07-04 02:05:16 +0530

    Update Count_Digits.py

--- Math_concept/Count_Digits.py
+++ Math_concept/Count_Digits.py
@@ -16,3 +16,23 @@ def count_digit(n):
     
 a = 23
 print(count_digit(a))
+
+
+
+#2nd method
+class Solution:
+    def countDigits(self, n):
+    #  code here 
+    
+        count = 0
+        original = n
+    
+        while n > 0:
+            ld = n % 10
+        
+            if ld >= 0:
+                count += 1
+            
+            n = n // 10
+    
+        return count
